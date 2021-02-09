44265621d7c2d84ba90ad5448af9eca4d436c597
Daniel Curilla <curilladaniel@gmail.com>
2021-02-08 18:30:46 -0700

    Form reset complete

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let library = [];
 const tbl = document.querySelector("div.table");
 const addBtn = document.querySelector("button.add");
+const clsBtn = document.querySelector("div.modal__close-btn");
+const form = document.querySelector("form.modal__form");
 
 addBtn.addEventListener("click",toggleModal);
+clsBtn.addEventListener("click",toggleModal);
 
 function toggleModal() {
     document.querySelector("div.overlay").classList.toggle("overlay--hidden");
     document.querySelector("div.modal").classList.toggle("modal--hidden");
+    form.reset();
 }
+
 
 function Book(title, author, pages, read=false){
     this.title = title;
